18d08d7a9c27fe041b7fbe9887574b78acce155e
Vishwesh <visudevaram@gmail.com>
2017-12-20 19:20:10 +0100

    Fixed Null pointer exception

--- CountryBO.java
+++ CountryBO.java
@@ -7,15 +7,17 @@ class CountryBO{
         return cnt1;
     }
 
-    public void displaySpecificCountryDetails(Country[] countryList, String countryName, int countryCount){
+    public void displaySpecificCountryDetails(Country[] countryList, String countryName, int countryCount) throws Exception{
         int i=0, j = 0;
         while(countryCount > 0) {
             String cntName = countryList[i].getName();
             System.out.println("Country names in array:-"+ cntName + "-");
             System.out.println("Country names in array:-"+ countryName + "-");
             if (cntName.equals(countryName)){
-                System.out.println("Found country");
+            	String pri = countryList[i].toString();
+                System.out.println("Found country" + countryList[i]);
                 j = 1;
+                break;
                 }
             else {
                 countryCount--;
@@ -26,5 +28,10 @@ class CountryBO{
         if(j==0){
             System.out.println("No country found");
         }
-
-}}
+        
+    }
+    public String toString() {
+    	Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
+    	gson.toJson(myObject);
+    }
+}
